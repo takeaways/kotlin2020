c597a59241ab1cd25f96826468441ad71279cad3
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-03-24 20:23:20 +0900

    함수 만들어 보기

---
+++ src/com/geoniljang/funtion/함수 001.kt
@@ -0,0 +1,9 @@
+package com.geoniljang.funtion
+
+fun main(args: Array<String>) {
+    println("Hello world")
+    val result = sum(10, 20);
+    println("10 + 20 $result")
+}
+
+fun sum(a:Int, b: Int):Int = a+b
--- src/studyTwo/human2Main.kt
+++
@@ -1,8 +0,0 @@
-package studyTwo
-
-fun main(args: Array<String>) {
-    val human = Human2();
-    human.age = -10;
-
-    println(human.age);
-}
