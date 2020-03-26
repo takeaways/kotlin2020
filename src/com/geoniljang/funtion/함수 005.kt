7767a969ba02b22e4c0a82eeaeafbec5c0d2714c
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-03-26 23:15:32 +0900

    add something

--- src/com/geoniljang/funtion/함수 005.kt
+++ src/com/geoniljang/funtion/함수 005.kt
@@ -4,29 +4,39 @@ fun main() {
     val result = callByValue2(lambda2)
     val result2 = callByValue2(::lambda3) // ::을 이용하면 기존의 함수를 람다 함수로 이용할 수 있다. -> 매개변수로 사용가능 리턴으로 사용가능 변수에 할당 가능 3개
     println(">>>>>>>>>>>>> $result2")
-    if(result){
+    if (result) {
         println("result가 정상 동작합니다.")
-    }else{
+    } else {
         println("result false 입니다.")
     }
 
 
-    mutiply(100 , 200)
+    mutiply(100, 200)
+    muti2(100,200)
+    nonReturnValue()
+    oneValue("GeonilJang")
 }
 
-fun callByValue2(b:()->Boolean):Boolean{
+fun callByValue2(b: () -> Boolean): Boolean {
     println("나는 콜 바이 값이다.")
     return b()
 }
 
-val lambda2 : () -> Boolean = {
+val lambda2: () -> Boolean = {
     println("나는 랍다 함수 입니다.");
     true
 }
 
-fun lambda3():Boolean{
+fun lambda3(): Boolean {
     return true
 }
 
-fun sumP(x :Int , y: Int): Int = x +y
-val mutiply :(Int, Int) -> Int = {x,y -> x*y}
+fun sumP(x: Int, y: Int): Int = x + y
+val mutiply: (Int, Int) -> Int = { x, y -> x * y }
+val muti2: (Int, Int) -> Int = { x, y ->
+    println("당신이 입력한 값은${x}, $y 입니다.")
+    x * y
+}
+val nonReturnValue : ()->Unit = {println("Hello World")}
+val oneValue : (String) -> Unit = {name -> println("Your name is ${name}.")}
+
